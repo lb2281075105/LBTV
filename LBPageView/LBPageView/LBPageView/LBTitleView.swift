93e0d1bd6232b149cfabffc5ec3d5e1f196db3d5
yunmei <2281075105@qq.com>
2018-02-10 09:06:35 +0800

    初始化分页视图

---
+++ LBPageView/LBPageView/LBPageView/LBTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  LBTitleView.swift
+//  LBPageView
+//
+//  Created by yunmei on 2018/2/10.
+//  Copyright © 2018年 liubo. All rights reserved.
+//
+
+import UIKit
+
+class LBTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LBPageView/LBPageView/LBPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LBPageView
+//
+//  Created by yunmei on 2018/2/6.
+//  Copyright © 2018年 liubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
