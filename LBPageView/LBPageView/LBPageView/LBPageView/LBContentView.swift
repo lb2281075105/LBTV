4b690d432ccfe0d903b4d225fbe637e7402c0037
lb2281075105 <2281075105@qq.com>
2018-03-05 16:16:28 +0800

    分页视图设置滑动

---
+++ LBPageView/LBPageView/LBPageView/LBPageView/LBContentView.swift
@@ -0,0 +1,21 @@
+//
+//  LBContentView.swift
+//  LBPageView
+//
+//  Created by liubo on 2018/3/5.
+//  Copyright © 2018年 liubo. All rights reserved.
+//
+
+import UIKit
+
+class LBContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
